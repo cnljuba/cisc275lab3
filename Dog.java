79b3dc3430f65b17cd3759fef2a5e8d937fdabe1
Casey Ljuba <casey.ljuba@gmail.com>
2019-09-12 19:14:49 -0400

    committing dog.java

---
+++ Dog.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dog extends Animal {
+
+	public Dog(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+}
